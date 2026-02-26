0caf1cdbb02acf24e7c08cf187d3eb7f5b31ff75
KIM-SUNGSOO <133596829+KIM-SUNGSOO@users.noreply.github.com>
2026-02-26 11:42:43 +0900

    Prac

--- CodingPractice/Program.cs
+++ CodingPractice/Program.cs
@@ -1,4 +1,31 @@
 using System;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+
+Foo.Car foocar = new Foo.Car();
+foocar.Go();
+Bar.Car barcar = new Bar.Car();
+barcar.Go();
+
+namespace Foo
+{
+    class Car
+    {
+        public void Go()
+        {
+            Console.WriteLine("Foo 네임스페이스의 Car 클래스");
+        }
+    }
+}
+
+namespace Bar
+{
+    class Car
+    {
+        public void Go()
+        {
+            Console.WriteLine("Bar 네임스페이스의 Car 클래스");
+        }
+    }
+}
--- GameSystem/Program.cs
+++ GameSystem/Program.cs
@@ -1,4 +1,64 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+using MyGame.Charaters;
+using MyGame.Items;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+Console.WriteLine("=== 캐릭터 시스템 ===");
+
+Player player = new Player();
+player.Attack();
+
+Enemy enemy = new Enemy();
+enemy.TakeDamage();
+
+Weapon weapon = new Weapon();
+weapon.GetDamage(50);
+
+Potion potion = new Potion();
+potion.Use();
+namespace MyGame.Charaters
+{
+    class Player
+    {
+        public string Name = "용사";
+
+        public void Attack()
+        {
+            Console.WriteLine($"플레이어 {Name}(이)가 공격합니다!");
+        }
+    }
+
+    class Enemy
+    {
+        public string Type = "고블린";
+
+        public void TakeDamage()
+        {
+            Console.WriteLine($"적 {Type}(이)가 데미지를 받았습니다!");
+        }
+    }
+}
+namespace MyGame.Items
+{
+    class Weapon
+    {
+        public string Name = "불꽃검";
+
+        public void GetDamage(int attack)
+        {
+            
+            Console.WriteLine($"무기 {Name}의 공격력: {attack}");
+        }
+    }
+    class Potion
+    {
+        public string Name = "체력 포션";
+
+        public void Use()
+        {
+            Console.WriteLine($"포션 {Name}을(를) 사용했습니다.");
+        }
+    }
+}
